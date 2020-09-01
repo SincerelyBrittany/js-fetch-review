d0368ff25f07d90cdb447acf9ceb69bc5a26c3ef
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-01 15:15:40 -0400

    adds pokemon array and obj functions

--- src/helpers.js
+++ src/helpers.js
@@ -1,1 +1,19 @@
-console.log("here")
+// console.log("here")
+const pokemonHTML = (obj) => {
+    return (`
+    <div class="pokemon-card">
+        <div class="pokemon-frame">
+            <h1 class="center-text">${obj.name}</h1>
+            <div class="pokemon-image">
+                <img data-id="${obj.id}" data-action="flip" class="toggle-sprite" src="${obj.sprites.front}">
+            </div>
+            <button data-action="delete" class="pokemon-button">Delete</button>
+        </div>
+    </div>`
+)}
+
+const pokemonArrayHTML = (arr) => {
+    // array of pokemon objects 
+   return arr.map(p => pokemonHTML(p)).join('')
+}
+
