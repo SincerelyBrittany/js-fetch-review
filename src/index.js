7cf6a0676e886d48a846f38e8781adb81b783b3f
SincerelyBrittany <sincerelybr@gmail.com>
2020-09-01 15:16:36 -0400

    adds fetch request to handle helper function and saves json to reference in future

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+    const container = document.querySelector('#pokemon-container')
+    const searchForm = document.querySelector('pokemon-search-form')
+    const postForm = document.querySelector('pokemon-post-form')
+    const baseURL = 'http://localhost:3000/pokemon'
+    fetch(baseURL)
+    .then(res => res.json())
+    .then(json => {
+         // console.log(json)
+        //  console.log(pokemonArrayHTML(json))
+        memoizedPokemon = json
+        container.innerHTML = pokemonArrayHTML(json)
+    })
 
 })
